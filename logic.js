b74606eadb12e0b39b5f5b51694c5161d67d5ee1
Ashiq <kashiq1999@gmail.com>
2021-02-09 15:50:39 +0530

    first commit

---
+++ logic.js
@@ -0,0 +1,9 @@
+/* let container =["rajasthan","tajmahal","delhi"];
+let c= document;
+c=document.links.href;
+c=document.images
+console.log(c); */
+let box = document.querySelector(".container");
+
+console.log(box.firstElementChild.nextElementSibling.innerText= "This is ashiq");
+console.log(box.firstElementChild.nextElementSibling.parentNode);
